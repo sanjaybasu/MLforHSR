3bf2cca21fbe85a59e8096b06401f3b8ad16c28f
sanjaybasu <sanjaybasu@users.noreply.github.com>
2017-09-12 15:05:51 -0700

    Add files via upload

--- R/reproduction.R
+++ R/reproduction.R
@@ -1,10 +1,9 @@
 # install.packages('caret') # uncomment these first install.packages lines if you don't already have these R packages
 # install.packages('DMwR')
-# install.packages('devtools')
-# install.packages('h2o')
-# NOTE: TO CORRECTLY ADMINISTER ENSEMBLE AND CUSTOM DEEP LEARNING SCRIPTS AS BELOW, IT IS IMPORTANT TO download GitHub desktop from github.com, click 'clone' at the h2o-3 repo (https://github.com/h2oai/h2o-3), go to out the "h2oEnsemble_v0.1.9" branch from in the GitHub program, cd to your local h2o-r/ensemble directory using your terminal window (mac) or command prompt (windows), then type in terminal: R CMD install h2oEnsemble-package ; SEE: https://community.h2o.ai/questions/1588/issue-with-dl-h2o-ensembles-and-h2o-last-version.html
+# install.packages('h2oEnsemble') # recommend cloning Github repo and installing to get newest version to avoid deep learning processing error
 # install.packages('pROC')
 rm(list=ls())
+options(warn=-1)
 pop = 1e5
 dummvars = 40 # binary dummy variables, e.g., yes/no for specific diagnostic codes
 secovars = 30 # secondary dummy variables, whose existence is influenced by the above-noted dummy variables, e.g., more likely to be diabetic if also obese and hypertensive and hyperlipidemic
@@ -38,8 +37,8 @@ influencers = seq(1,dim,round(dim/10)) # choosing which variables are truly infl
 set.seed(1000)
 coefs = runif(length(influencers))
 set.seed(1100)
-# simulating probability of the primary outcome based on influential variables, plus some complex interactions among other variables, producing positive and negative heterogeneity, and adding some noise
-outprob = (rowMeans(coefs*covars[,influencers])/dim+covars[,round(3*dim/20)]*covars[,round(4*dim/20)]/2-covars[,round(5*dim/20)]*covars[,round(7*dim/20)]/2+rnorm(pop, mean =0, sd = .01)) 
+# simulating probability of the primary otucome based on influential variables, plus some complex interactions among other variables, producing positive and negative heterogeneity, and adding some noise
+outprob = (rowMeans(coefs*covars[,influencers])/dim+covars[,round(3*dim/20)]*covars[,round(4*dim/20)]/2-covars[,round(5*dim/20)]*covars[,round(7*dim/20)]/2+covars[,round(3*dim/20)]*covars[,round(4*dim/20)]/2*covars[,round(5*dim/20)]*covars[,round(7*dim/20)]/2+rnorm(pop, mean =0, sd = .01)) 
 outprob[outprob>1]=1
 outprob[outprob<0]=0
 set.seed(1200)
@@ -48,7 +47,6 @@ alldata = data.frame(cbind(x1,x2,x3,x4,y))
 colnames(alldata) = c(paste("X",c(1:dim),sep=""),"y")
 alldata$y = factor(alldata$y)
 library(caret)
-options(warn=-1)
 set.seed(1300)
 splitIndex <- createDataPartition(alldata$y, p = .5, list = FALSE, times = 1) # randomly splitting the data into train and test sets
 trainSplit <- alldata[ splitIndex,]
@@ -59,7 +57,7 @@ set.seed(1400)
 trainSplit <- SMOTE(y ~ ., trainSplit, perc.over = 100, perc.under=200) # making a balanced subset of the training data, which is known to improve ML methods' performance
 prop.table(table(trainSplit$y))
 prop.table(table(testSplit$y))
-library(h2oEnsemble)  # NOTE: TO CORRECTLY ADMINISTER ENSEMBLE AND CUSTOM DEEP LEARNING SCRIPTS AS BELOW, IT IS IMPORTANT TO download GitHub desktop from github.com, click 'clone' at the h2o-3 repo (https://github.com/h2oai/h2o-3), go to out the "h2oEnsemble_v0.1.9" branch from in the GitHub program, cd to your local h2o-r/ensemble directory using your terminal window (mac) or command prompt (windows), then type in terminal: R CMD install h2oEnsemble-package ; SEE: https://community.h2o.ai/questions/1588/issue-with-dl-h2o-ensembles-and-h2o-last-version.html
+library(h2oEnsemble)  
 h2o.init(nthreads=-1, max_mem_size="50G")
 h2o.removeAll() 
 train <- as.h2o(trainSplit)
@@ -92,12 +90,11 @@ h2o.deeplearning.7 <- function(..., hidden = c(100,100), activation = "MaxoutWit
 h2o.glm_nn <- function(..., non_negative = T) h2o.glm.wrapper(..., non_negative = non_negative) # define meta-learner [GLM restricted to non-neg weights, which is shown in the literature to improve outcomes from ensembles]
 # ensemble choosing the training data, list of learners defined above, and meta-learner 
 fit <- h2o.ensemble(x = x, y = y,training_frame = train,family = "binomial",learner = c("h2o.glm.1","h2o.glm.2","h2o.glm.3","h2o.randomForest.1", "h2o.randomForest.2","h2o.randomForest.3","h2o.gbm.1","h2o.gbm.2","h2o.gbm.3","h2o.gbm.4","h2o.gbm.5","h2o.gbm.6","h2o.gbm.7","h2o.gbm.8","h2o.deeplearning.1","h2o.deeplearning.2","h2o.deeplearning.3","h2o.deeplearning.4","h2o.deeplearning.5","h2o.deeplearning.6", "h2o.deeplearning.7"),metalearner = "h2o.glm_nn",cvControl = list(V = 5))
-# NOTE: if you get an error "Error in h2o.deeplearning(x = x, y = y, training_frame = training_frame,  : unused argument (max_confusion_matrix_size = max_confusion_matrix_size)", you need to download GitHub desktop from github.com, click 'clone' at the h2o-3 repo (https://github.com/h2oai/h2o-3), go to out the "h2oEnsemble_v0.1.9" branch from in the GitHub program, cd to your local h2o-r/ensemble directory using your terminal window (mac) or command prompt (windows), then type in terminal: R CMD install h2oEnsemble-package ; SEE: https://community.h2o.ai/questions/1588/issue-with-dl-h2o-ensembles-and-h2o-last-version.html
-h2o.ensemble_performance(fit, newdata = test) # C-stat for each ML method and ensemble
+perfml <- h2o.ensemble_performance(fit, newdata = test) # assessing cross-validated C-stats
 fulllogmodel = glm(y~., data =trainSplit, family=binomial()) # conventional logistic model with all predictors
 aiclogmodel = step(fulllogmodel,trace=F) # conventional logistic model with backwards variable selection by AIC
-library(pROC)
-perffulllog = predict.glm(fulllogmodel,newdata=testSplit) # apply conventional logistic models to test data
-perfaiclog = predict.glm(aiclogmodel,newdata=testSplit)
-roc(testSplit$y, perffulllog) # c-stat for conventional logistic model with all predictors
-roc(testSplit$y, perfaiclog) # c-stat for conventional logistic model with backwards variable selection by AIC
+library(cvAUC)
+perffulllog = predict.glm(fulllogmodel,newdata=testSplit) # apply conventional logistic model with all predictors to test data
+perfaiclog = predict.glm(aiclogmodel,newdata=testSplit) # apply conventional logistic model with backwards variable selection by AIC to test data
+perffull <- cvAUC(perffulllog, testSplit$y) # cross-validated C-stats for conventional logistic model with all predictors
+perfaic <- cvAUC(perfaiclog, testSplit$y) # cross-validated C-stats for conventional logistic model with backwards variable selection by AIC
